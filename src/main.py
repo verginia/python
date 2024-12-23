94c0b8d1697131706cc3b72e7f2bef81bec6302c
verginia <geenika@gmail.com>
2024-12-23 19:49:14 +0200

    Users routes done

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from src.routes import router
 from src.database import Base, engine
+
+# Get-Process python* | Select-Object Id,ProcessName
+# Get-Process python* | Stop-Process -Force
+
 # Create database tables
 Base.metadata.create_all(bind=engine)
 
